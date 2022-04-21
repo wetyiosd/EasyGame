f83e74393623a6f4330ed592a9b0d937addef0fe
mobile <>
2022-04-21 16:40:06 +0500

    готовый проект

--- app/src/main/java/com/example/is2011_igra/MainActivity.java
+++ app/src/main/java/com/example/is2011_igra/MainActivity.java
@@ -5,21 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button Play;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Play = (Button) findViewById(R.id.button);
+        Play.setOnClickListener(this);
+
     }
 
     public void onClick(View view) {
-        // Создаем объект Intent для вызова новой Activity
-        Intent intent = new Intent(this,GameActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-        startActivity(intent);
+        switch (view.getId()) {
+            case R.id.button:
+                Intent intent = new Intent(this, GameActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 
 }
